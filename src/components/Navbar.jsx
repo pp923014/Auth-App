ea3e69bd53bc39f264080ac65e6397785039f039
priyanshu Kumar Pandey <06priyanshupandey@gmail.com>
2025-06-27 10:19:16 +0530

    navbar update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,9 +12,13 @@ export default function Navbar() {
 
   return (
     <nav className="bg-white shadow-md px-6 py-3 flex justify-between items-center">
-      <Link to="/" className="text-xl font-bold text-blue-600">
-        AuthApp
-      </Link>
+      {user ? (
+        <h1 className="text-xl font-bold text-blue-600">AuthApp</h1>
+      ) : (
+        <Link to="/" className="text-xl font-bold text-blue-600">
+          AuthApp
+        </Link>
+      )}
 
       <div>
         {user ? (
